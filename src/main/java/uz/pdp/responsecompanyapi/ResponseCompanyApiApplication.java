2a99cae039f69738a3b59b0ab51798a3ca3e4585
IbrokhimGitHub <i.irmukhamedov@gmail.com>
2021-08-07 10:17:32 +0500

    firstCommit

---
+++ src/main/java/uz/pdp/responsecompanyapi/ResponseCompanyApiApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.responsecompanyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResponseCompanyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResponseCompanyApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/responsecompanyapi/ResponseCompanyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.responsecompanyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResponseCompanyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
